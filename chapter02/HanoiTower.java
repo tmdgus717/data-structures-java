5ffadb72b54d34c617b72aa761871c96803e50e9
sean <tmdgus717@naver.com>
2024-08-11 00:50:12 +0900

    chapter03 시작

--- chapter02/HanoiTower.java
+++ chapter02/HanoiTower.java
@@ -3,7 +3,7 @@ package chapter02;
 public class HanoiTower {
 
     private static void hanoiTowerMove(int n, char a, char b, char c) {
-        if (n == 1) System.out.println("원반 " + n + "을 " + a + "에서 " + c + "로 이동");
+        if (n == 1) System.out.println("원반 " + 1 + "을 " + a + "에서 " + c + "로 이동");
         else {
             hanoiTowerMove(n - 1, a, c, b);
             System.out.println("원반 " + n + "을 " + a + "에서 " + c + "로 이동");
---
+++ chapter03/chapter03.md
@@ -0,0 +1,4 @@
+## chapter 03
+### 03-1 추상 자료형 : Abstract Data Type
+- 추상 자료형(ADT)
+- 
